59c98324b6cd7df0db033a11a501ab59b2762bb4
kang <dev.kangcho@gmail.com>
2026-05-01 17:38:23 +0900

    sql

--- sql/04_clean_property.sql
+++ sql/04_clean_property.sql
@@ -3,8 +3,30 @@ WITH (
   format = 'PARQUET',
   external_location = '{output_path}'
 ) AS
-SELECT *
+SELECT
+  address,
+  suburb,
+  price,
+  bedrooms,
+  bathrooms,
+  garage,
+  land_area,
+  floor_area,
+  cbd_dist,
+  nearest_stn,
+  nearest_stn_dist,
+  date_sold,
+  postcode,
+  latitude,
+  longitude,
+  nearest_sch,
+  nearest_sch_dist,
+  nearest_sch_rank
 FROM wa_property_db.wa_property_raw
 WHERE bedrooms <= 5
   AND bathrooms <= 3
-  AND garage <= 2;
+  AND garage <= 2
+  AND land_area BETWEEN 400 AND 800
+  AND date_parse(date_sold, '%m-%Y') >= DATE '2015-01-01'
+
+ORDER BY postcode;

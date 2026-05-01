d443d6fe68ea56c64305e88dc6cd7ea8a3f28914
kang <dev.kangcho@gmail.com>
2026-05-01 16:20:40 +0900

    sql revise

--- sql/01_create_raw_table.sql
+++ sql/01_create_raw_table.sql
@@ -1,7 +1,7 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS wa_property_db.wa_property_raw (
+CREATE EXTERNAL TABLE wa_property_db.wa_property_raw (
   address string,
   suburb string,
-  price double,
+  price int,
   bedrooms int,
   bathrooms int,
   garage int,
@@ -17,15 +17,16 @@ CREATE EXTERNAL TABLE IF NOT EXISTS wa_property_db.wa_property_raw (
   longitude double,
   nearest_sch string,
   nearest_sch_dist double,
-  nearest_sch_rank double
+  nearest_sch_rank int
 )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
   'separatorChar' = ',',
-  'quoteChar' = '"',
-  'escapeChar' = '\\'
+  'quoteChar' = '"'
 )
+STORED AS TEXTFILE
 LOCATION 's3://personal-wa-property-storage-337164669284-ap-southeast-2-an/raw/property/'
 TBLPROPERTIES (
-  'skip.header.line.count'='1'
+  'skip.header.line.count'='1',
+  'use.null.for.invalid.data'='true'
 );
